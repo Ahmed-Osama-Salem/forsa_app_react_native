5489d1162d97b9dd951873edc1e201f1aa4bd378
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-05-25 23:16:08 +0300

    add folder structure

---
+++ app/components/modules/HeaderSample.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {Text, View} from 'react-native';
+
+const HeaderSample = () => {
+  return (
+    <View>
+      <Text>Top brands Retail</Text>
+    </View>
+  );
+};
+
+export default HeaderSample;
